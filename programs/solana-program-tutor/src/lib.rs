67a06d9a5c38353b2592a3713507eed8bc6d9dce
block-hac <cn.corestar@gmail.com>
2022-07-13 00:46:59 +0800

    update to build and add targe

--- programs/solana-program-tutor/src/lib.rs
+++ programs/solana-program-tutor/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::TokenAccount;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +7,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_program_tutor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn set_data(ctx: Context<SetData>, data:u64) -> Result<()> {
+      if ctx.accounts.token_account.amount > 0 {
+        ctx.accounts.my_account.data = data;
+      }
+      Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SetData<'info> {
+  #[account(mut)]
+  pub my_account: Account<'info, MyAccount>,
+  #[account(constraint = my_account.mint == token_account.mint, has_one = owner)]
+  pub token_account: Account<'info, TokenAccount>,
+  pub owner: Signer<'info>
+}
+
+#[account]
+#[derive(Default)]
+pub struct MyAccount {
+  data: u64,
+  mint: Pubkey
+}
--- tests/solana-program-tutor.ts
+++ tests/solana-program-tutor.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaProgramTutor } from "../target/types/solana_program_tutor";
+import assert from "assert";
+
+const { SystemProgram } = anchor.web3;
 
 describe("solana-program-tutor", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.SolanaProgramTutor as Program<SolanaProgramTutor>;
+  const program = anchor.workspace
+    .SolanaProgramTutor as Program<SolanaProgramTutor>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creates a counter", async() => {
+    const baseAccount = anchor.web3.Keypair.generate();
+    
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 0:', account.count.toString());
+    assert.ok(account.count.toString() == 0);
+    _baseAccount = baseAccount;
   });
+
+  it("Increments the counter", async () => {
+    const baseAccount = _baseAccount;
+    
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 1: ', account.count.toString());
+    assert.ok(account.count.toString() == 1);
+  })
 });
